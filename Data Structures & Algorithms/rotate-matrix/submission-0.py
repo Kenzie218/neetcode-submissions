cc861202bdfa44a4e1b049288b0af9872eaeacd5
Makenzie Johnson <mvolleyballjohnson@gmail.com>
2026-05-27 22:15:26 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+        size = len(matrix)
+
+        for i in range(size):
+            for j in range(i + 1, size):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+            matrix[i].reverse()
+                
+                
+
+        print(matrix)
