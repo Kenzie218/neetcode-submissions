9b4652078e909ce98659fe4c94be2a6ce4c6947f
Makenzie Johnson <mvolleyballjohnson@gmail.com>
2026-06-01 14:12:47 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        new_s = ''.join(filter(str.isalnum,s)).lower() #removed spaces
+
+        i = 0
+        j = len(new_s) - 1
+
+        for i in range(len(new_s)):
+            if new_s[i] != new_s[j]:
+                return False
+            j -= 1
+        return True
